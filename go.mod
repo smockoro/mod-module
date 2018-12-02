f50a3cec34ae0a367e6d584f277e2a59a33d84a7
Takahiro Oshima <tarotora51@gmail.com>
2018-12-02 18:57:58 +0900

    first

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/smockoro/mod-module
+
+require (
+	github.com/smockoro/mod-account v1.0.0
+	github.com/smockoro/mod-shop v1.0.2
+)
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	account "github.com/smockoro/mod-account/account"
+	shop "github.com/smockoro/mod-shop/shop"
+)
+
+func main() {
+	account.AccountHello()
+	shop.ShopHello()
+}
